88c86c36ecaf7917a2d2207d3826184dd182b6a2
Timothy Shores <timothymshores@gmail.com>
2019-05-16 16:28:51 -0500

    Configured helmet as middleware in server.js, create users/users-model, create users/users-router and import users-router into server.js

---
+++ users/users-model.js
@@ -0,0 +1,14 @@
+const db = require('../data/dbConfig');
+
+module.exports = {
+    insert,
+    remove
+};
+
+async function insert(user) {
+    return null;
+}
+
+function remove(id) {
+    return null;
+}
---
+++ users/users-router.js
@@ -0,0 +1,4 @@
+const router = require('express').Router();
+const Users = require('./users-model');
+
+module.exports = router;
