0042afe9cd0a89ac7aa2bd879024384fecf9ad1c
Timothy Shores <timothymshores@gmail.com>
2019-05-16 16:07:23 -0500

    Add an auto-incrementing primary key id and a unique not null name field to users table migration

--- data/migrations/20190516160408_users.js
+++ data/migrations/20190516160408_users.js
@@ -1,8 +1,11 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('users', tbl => {
+        tbl.increments();
+        tbl.string('name', 128).notNullable().unique();
+    });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+    return knex.schema
+        .dropTableIfExists('users')
 };
